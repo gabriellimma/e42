b5683ac73caa284ea1fca969b56c996b37ad10f2
LuxVitz <limma.lucas@gmail.com>
2021-10-04 23:48:21 -0300

    subindo basico-de-ssh.sh issue #1

---
+++ basics/basico-de-ssh.sh
@@ -0,0 +1,7 @@
+# O SSH é um protocolo que garante que cliente
+# e servidor remoto troquem informações de 
+# maneira segura e dinâmica. O processo é capaz 
+# de criptografar os arquivos enviados ao 
+# diretório do servidor, garantindo que 
+# alterações e o envio de dados sejam realizados
+# da melhor forma.
